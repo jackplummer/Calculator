e7ad3892216fd5f3d3b2f43540d565ab2cdecd35
Jack Plummer <jackplummer78@gmail.com>
2021-10-08 15:53:36 +0100

    Finished calculator logic with working decimal point & CSS styling

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Calculator
 A basic calculator created using HTML, CSS and Javascript. Inspired by The Odin Project Foundations Course, demonstrating my skills in HTML, CSS and JavaScript.
+
+The buttons are arranged using CSS-grid. A great tutorial for this layout is: https://freshman.tech/css-grid-calculator/.
+
+The font used for the calculator display is free for personal use. It has been reproduced from allfont.net and is created by Sizenko Alexander. © 2008 Style-7. All rights reserved. http://www.styleseven.com.
+
+ 
--- javascript.js
+++ javascript.js
@@ -3,15 +3,23 @@ const subtract = (a,b) => a - b;
 const multiply = (a,b) => a * b;
 const divide = (a,b) => a / b;
 
+function roundTwoDecimal(number) {
+  return Math.round(number * 100) / 100;
+}
+
 function operate(a,b,operation) {
-  if (operation === add) {
+  a = Number(a);
+  b = Number(b);
+  if (operation === 'add') {
     return add(a,b);
-  } else if (operation === subtract) {
+  } else if (operation === 'subtract') {
     return subtract(a,b);
-  } else if (operation === multiply) {
+  } else if (operation === 'multiply') {
     return multiply(a,b);
-  } else if (operation === divide) {
+  } else if (operation === 'divide') {
     return divide(a,b);
+  } else if (operation === 'null') {
+    return a;
   };
 };
 
@@ -37,39 +45,67 @@ for (let i = 0; i < operations.length; i++) {
   });
 };
 
+function decimalCheck(a,b,operation) { // handles decimal and avoids double decimal
+  if (operation === 'null') {
+    if (/\./.test(a)) {
+      console.log('decimal found');
+      return '';
+    } else {
+      console.log('decimal not found');
+      firstOperand += '.';
+      return '.';
+    };
+  } else {
+    if (/\./.test(b)) {
+      console.log('decimal found');
+      return '';
+    } else {
+      console.log('decimal not found');
+      secondOperand += '.';
+      return '.';
+    };
+  };
+};
+
 const display = document.querySelector('.display');
 function displayChange(input) {
   if (input === 'clear') {
     display.textContent = '';
-    counter = 0;
+    operationStored = 'null';
     firstOperand = 0;
     secondOperand = 0;
+  } else if (input === 'equals'){
+    answer = roundTwoDecimal(operate(firstOperand,secondOperand,operationStored));
+    display.textContent = `= ${answer}`
+    firstOperand = answer;
+    secondOperand = 0;
+    operationStored = 'null';
   } else if (input === 'add'){
     display.textContent += ' + ';
+    operationStored = 'add';
   } else if (input === 'subtract'){
     display.textContent += ' - ';
+    operationStored = 'subtract';
   } else if (input === 'multiply'){
     display.textContent += ' * ';
+    operationStored = 'multiply';
   } else if (input === 'divide'){
     display.textContent += ' / ';
+    operationStored = 'divide';
+  } else if (input === 'decimal') {
+    display.textContent += decimalCheck(firstOperand,secondOperand,operationStored);
   } else {
     display.textContent += `${input}`;
-    if (counter === 0) {
+    if (operationStored === 'null') {
       firstOperand += `${input}`;
     } else {
       secondOperand += `${input}`;
     };
   }};
 
-  // calculator logic
-  // store first number
-  // store operation (in counter?)
-  // store second number
-  // call operate once equals is pressed
-  // display the answer on the display
-
   let firstOperand = 0;
   let secondOperand = 0;
-  let counter = 0;
+  let operationStored = 'null';
+  let answer = 0;
 
 
